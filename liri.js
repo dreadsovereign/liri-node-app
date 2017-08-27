a9ab2bc799de00382c2fa314caaafa57cc75e7ac
Kamran <narmak182@gmail.com>
2017-08-27 18:45:18 -0400

    Added headers to info sections when called. Removed /n from dashes to reduce space between tweets etc.

--- liri.js
+++ liri.js
@@ -5,15 +5,13 @@ var request = require("request");
 var twitter = require("twitter");
 
 var keys = require("./keys.js");
-
 var client = new twitter(keys.twitterKeys);
-var spotify = new Spotify(keys.spotifykeys);
 
 var input1 = process.argv[2];
 var input2 = process.argv.slice(3).join(" ");
 
 var Spotify = require("node-spotify-api");
-
+var spotify = new Spotify(keys.spotifykeys);
 
 liri ();
 
@@ -31,7 +29,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     tweets.forEach(function(individualTweet) {
           console.log('Time Posted: ' + individualTweet.created_at);
           console.log('Tweet: ' + individualTweet.text);
-          console.log("\n--------------------------------------------\n");
+          console.log("--------------------------------------------");
       })
     }
   })
@@ -44,13 +42,14 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
  if (err) {
    return console.log('Error occurred: ' + err);
   }
- var spotifyinfo = data.tracks.items[0];
- console.log("\n--------------------------------------------\n");
- console.log("Artist: " + spotifyinfo.artists[0].name); 
- console.log("Song: " + spotifyinfo.name);
- console.log("Preview: " + spotifyinfo.preview_url);
- console.log("Album: " + spotifyinfo.album.name);
- console.log("\n--------------------------------------------\n");
+   var spotifyinfo = data.tracks.items[0];
+   console.log("Spotify Song Information:")
+   console.log("--------------------------------------------");
+   console.log("Artist: " + spotifyinfo.artists[0].name); 
+   console.log("Song: " + spotifyinfo.name);
+   console.log("Preview: " + spotifyinfo.preview_url);
+   console.log("Album: " + spotifyinfo.album.name);
+   console.log("--------------------------------------------");
 });
 
 } else if (input1 === "movie-this") {
@@ -61,7 +60,8 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   request("http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&r=json&tomatoes=true&apikey=40e9cece", function(error, response, body) {
     if (!error && response.statusCode === 200) {
       var omdbinfo = JSON.parse(body);
-      console.log("\n--------------------------------------------------------------------\n");
+      console.log("OMDb Movie Information:")
+      console.log("--------------------------------------------------------------------");
       console.log("Title: " + omdbinfo.Title);
       console.log("Year: " + omdbinfo.Year);
       console.log("IMDb Rating: " + omdbinfo.imdbRating);
@@ -69,7 +69,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
       console.log("Country: " + omdbinfo.Country);
       console.log("Plot: " + omdbinfo.Plot);
       console.log("Actors: " + omdbinfo.Actors);
-      console.log("\n--------------------------------------------------------------------\n");
+      console.log("--------------------------------------------------------------------");
     }
   })
 } else if (input1 === "do-what-it-says") {
