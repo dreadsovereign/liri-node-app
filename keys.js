d32f67722770a22e2ff35206f207b74773b66279
Kamran <narmak182@gmail.com>
2017-08-26 21:01:22 -0400

    Almost finished. Code works

--- keys.js
+++ keys.js
@@ -1,8 +1,13 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
 exports.twitterKeys = {
   consumer_key: 'U53irrdvfxfFUlzY580oUlK3W',
   consumer_secret: 'YiXEirTqDzH82FGRGefdkOzbfZGZpxqwIb2rPcWe5jPtbiNhRE',
   access_token_key: '901198938006589440-HujHkhZAdc6pXHpiF4gYPo8ZPVEidoB',
   access_token_secret: 'iuw19qV47dAd5dMShoHPYKY8KnkPYmDPPUljgaomZL9ou',
+}
+
+exports.spotifykeys = {
+  id: "feed762f588442ff977f96d4934e29a7",
+  secret: "cb6b21e0dca349f19151bfae1dfd5cc2"
 }
--- liri.js
+++ liri.js
@@ -1,30 +1,87 @@
-var request = require("request")
+var fs = require("fs");
+var request = require("request");
 
-var Twitter = require("twitter");
+var twitter = require("twitter");
 
-var twitterKeys = require("./keys.js");
-var client = twitterKeys.twitterKeys;
+var keys = require("./keys.js");
+
+var client = new twitter(keys.twitterKeys);
+
+var input1 = process.argv[2];
+var input2 = process.argv.slice(3).join(" ");
 
 var Spotify = require("node-spotify-api");
- 
-var spotify = new Spotify({
-  id: "feed762f588442ff977f96d4934e29a7",
-  secret: "cb6b21e0dca349f19151bfae1dfd5cc2"
-});
+var spotify = new Spotify(keys.spotifykeys);
 
-//spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
- // if (err) {
- //   return console.log('Error occurred: ' + err);
- // }
- 
-//console.log(data); 
-//});
+liri ();
 
-var params = {screen_name: 'webdeveloper82'};
+function liri () {
+
+if (input1 === "my-tweets") {
+
+var params = {
+    screen_name: 'webdeveloper82',
+    count: 20
+};
 
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    tweets.forEach(function(individualTweet) {
+          console.log('Time Posted: ' + individualTweet.created_at);
+          console.log('Tweet: ' + individualTweet.text);
+          console.log("\n--------------------------------------------\n");
+      })
+    }
+  })
+} else if (input1 === "spotify-this-song") {
+  if (input2.length < 1) {
+
+            input2 = "The Sign Ace of Base";
+        };
+ spotify.search({ type: 'track', query: input2 }, function(err, data) {
+ if (err) {
+   return console.log('Error occurred: ' + err);
   }
+ console.log("\n--------------------------------------------\n");
+ console.log("Artist: " + data.tracks.items[0].artists[0].name); 
+ console.log("Song: " + data.tracks.items[0].name);
+ console.log("Preview: " + data.tracks.items[0].preview_url);
+ console.log("Album: " + data.tracks.items[0].album.name);
+ console.log("\n--------------------------------------------\n");
 });
 
+} else if (input1 === "movie-this") {
+  if (input2.length < 1) {
+
+      input2 = "Mr. Nobody";
+  };
+  request("http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&r=json&tomatoes=true&apikey=40e9cece", function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log("\n--------------------------------------------------------------------\n");
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Year: " + JSON.parse(body).Year);
+      console.log("IMDb Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Country: " + JSON.parse(body).Country);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      console.log("\n--------------------------------------------------------------------\n");
+    }
+  })
+} else if (input1 === "do-what-it-says") {
+  
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+  if (error) {
+    return console.log(error);
+  }
+  
+  var dataArr = data.split(",");
+
+  input1 = dataArr[0];
+  input2 = dataArr[1];
+  liri();
+
+    });
+  }
+};
